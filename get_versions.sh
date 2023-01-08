416a5a2cb25110a47737d2241fef8ccdad07a654
Paul Rosania <paul.rosania@gmail.com>
2023-01-07 17:51:49 -0800

    remove extra newline from `version`

--- get_versions.sh
+++ get_versions.sh
@@ -4,6 +4,6 @@ set -e
 
 mkdir -p tmp
 
-go version | { read _ _ v _; echo ${v#go}; } > tmp/go_version.txt
+go version | { read _ _ v _; echo -n ${v#go}; } > tmp/go_version.txt
 git describe --tags --dirty | tr -d '\n' > tmp/version.txt
 git rev-parse --short HEAD | tr -d '\n' > tmp/commit_hash.txt
